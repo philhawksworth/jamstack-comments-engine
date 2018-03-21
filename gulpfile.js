ad071c1825d98a60392e6231fe762d1538d8a0f9
Phil Hawksworth <phil@netify.com>
2018-03-21 15:17:07 +0000

    basic comment form added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var gulp  = require("gulp");
-var sass  = require("gulp-sass");
-var serve = require('gulp-serve');
-var shell = require('gulp-shell');
+var gulp      = require("gulp");
+var sass      = require("gulp-sass");
+var serve     = require('gulp-serve');
+var shell     = require('gulp-shell');
+var request   = require("request");
+var gravatar  = require('gravatar');
 
 // what goes where?
 var buildSrc = "src";
@@ -29,7 +31,63 @@ gulp.task("scss", function () {
 gulp.task('generate', shell.task('eleventy --config=eleventy.js'));
 
 
-// TODO: get comments from Netlify submissions API into data files
+
+
+// Collect and stash comments for the build
+gulp.task("get:comments", function () {
+
+  // set up our request with appropriate auth token and Form ID
+  var oauth_token = process.env.NETLIFY_TOKEN;
+  var formID = "5a6df445ae52900fdc164e26";
+  var url = "https://api.netlify.com/api/v1/forms/" + formID + "/submissions/?access_token=" + oauth_token;
+
+  // Go and get the data from Netlify's submissions API
+  request(url, function(err, response, body){
+    if(!err && response.statusCode === 200){
+      var body = JSON.parse(body);
+      var comments = {};
+      for(var item in body){
+        var data = body[item].data;
+        var comment = {
+          name: data.name,
+          avatar: gravatar.url(data.email, {s: '100', r: 'x', d: 'retro'}, true),
+          comment: data.comment,
+          date: body[item].created_at
+        };
+        // Add it to an existing array or create a new one
+        if(comments[data.path]){
+          comments[data.path].push(comment);
+        } else {
+          comments[data.path] = [comment];
+        }
+      }
+
+      // store all of the organised comments in a yaml file keyed by the path for each comment
+      var commentFile = "/data/comments.json";
+
+      // if(body.length === 0) {
+      //   ymlText = "---"
+      //   console.log("No comments :( ");
+      // } else {
+      //   var ymlText = yaml.stringify(comments);
+      //   console.log("There are comments to stash");
+      // }
+
+      // write our data to a file where our site generator can get it.
+      fs.writeFile(__dirname + commentFile, comments, function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Comments data saved.");
+        }
+      });
+
+    } else {
+      console.log("Couldn't get comments from Netlify");
+    }
+  });
+});
+
 
 
 // Watch src folder for changes
