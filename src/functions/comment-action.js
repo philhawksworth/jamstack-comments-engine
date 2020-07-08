c1f1bad6e7e0e48a707f96c7e8e5d49a114f4f11
Phil Hawksworth <phil@netlify.com>
2020-07-08 18:31:20 +0100

    replace request with axios

--- src/functions/comment-action.js
+++ src/functions/comment-action.js
@@ -1,4 +1,4 @@
-var request = require("request");
+const axios = require('axios');
 
 // populate environment variables locally.
 require('dotenv').config()
@@ -12,14 +12,13 @@ const {
   delete this submission via the api
 */
 function purgeComment(id) {
-  var url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`;
-  request.delete(url, function(err, response, body){
-    if(err){
-      return console.log(err);
-    } else {
-      return console.log("Comment deleted from queue.");
-    }
-  });
+  axios.delete(`https://api.netlify.com/api/v1/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`)
+    .then(function(){
+      console.log("Comment deleted from queue.");
+    })
+    .catch(function(error){
+      console.log(err);
+    });
 }
 
 
@@ -40,16 +39,17 @@ export function handler(event, context, callback) {
       statusCode: 200,
       body: "Comment deleted"
     });
-  } else if (method == "approve"){
+  }
+  else if (method == "approve") {
 
     // get the comment data from the queue
-    var url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`;
+    axios.get(`https://api.netlify.com/api/v1/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`)
+      .then(function(response){
 
-    request(url, function(err, response, body){
-      if(!err && response.statusCode === 200){
-        var data = JSON.parse(body).data;
+        console.log('get submission response :>> ', response);
 
         // now we have the data, let's massage it and post it to the approved form
+        var data = response;
         var payload = {
           'form-name' : "approved-comments",
           'path': data.path,
@@ -63,25 +63,22 @@ export function handler(event, context, callback) {
         console.log(payload);
 
         // post the comment to the approved lost
-        request.post({'url':URL, 'formData': payload }, function(err, httpResponse, body) {
-          var msg;
-          if (err) {
-            msg = 'Post to approved comments failed:' + err;
-            console.log(msg);
-          } else {
+        axios.post(URL, payload)
+          .then(function () {
             msg = 'Post to approved comments list successful.'
             console.log(msg);
             purgeComment(id);
-          }
-          var msg = "Comment registered. Site deploying to include it.";
-          callback(null, {
-            statusCode: 200,
-            body: msg
           })
-          return console.log(msg);
-        });
-      }
-    });
+          .catch(function (error) {
+            msg = 'Post to approved comments failed:' + error;
+            console.log(msg);
+          });
 
+      })
+      .catch(function (error) {
+        console.log("Failed to get submission data");
+        console.log(error);
+      })
+
+    }
   }
-}
