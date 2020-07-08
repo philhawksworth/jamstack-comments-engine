463d8840ceef89b33b80e3d3e718e9e8ad6578db
Phil Hawksworth <phil@netlify.com>
2020-07-08 18:48:28 +0100

    clean up env vars

--- src/functions/comment-handler.js
+++ src/functions/comment-handler.js
@@ -1,9 +1,11 @@
-'use strict';
-
-var request = require("request");
+var axios = require("axios");
 
 // populate environment variables locally.
 require('dotenv').config()
+const {
+  SLACK_WEBHOOK_URL,
+  URL
+} = process.env;
 
 
 /*
@@ -15,20 +17,19 @@ export function handler(event, context, callback) {
   var body = JSON.parse(event.body);
 
   // prepare call to the Slack API
-  var slackURL = process.env.SLACK_WEBHOOK_URL
   var slackPayload = {
-    "text": "New comment on " + process.env.URL,
+    "text": "New comment on " + URL,
 	  "attachments": [
       {
         "fallback": "New comment on the comment example site",
         "color": "#444",
         "author_name": body.data.email,
         "title": body.data.path,
-        "title_link": process.env.URL + body.data.path,
+        "title_link": URL + body.data.path,
         "text": body.data.comment
       },
       {
-        "fallback": "Manage comments on " + process.env.URL,
+        "fallback": "Manage comments on " + URL,
         "callback_id": "comment-action",
         "actions": [
           {
@@ -49,18 +50,21 @@ export function handler(event, context, callback) {
     };
 
     // post the notification to Slack
-    request.post({url:slackURL, json: slackPayload}, function(err, httpResponse, body) {
-      var msg;
-      if (err) {
-        msg = 'Post to Slack failed:' + err;
-      } else {
-        msg = 'Post to Slack successful!  Server responded with:' + body;
-      }
+    let msg;
+    axios.post(SLACK_WEBHOOK_URL, slackPayload)
+    .then(function(response){
+      msg = 'Post to Slack successful!  Server responded with:' + body;
+    })
+    .catch(function(error){
+      msg = 'Post to Slack failed:' + err;
+    })
+    .finally(function(){
+      console.log(msg);
       callback(null, {
         statusCode: 200,
         body: msg
       })
-      return console.log(msg);
     });
+
 
 }
