e885028774b3ea8c7abb3028030d2513bbf641d3
Phil Hawksworth <phil@netlify.com>
2020-07-09 12:23:37 +0100

    update onboarding help

--- src/site/_data/environment.js
+++ src/site/_data/environment.js
@@ -4,16 +4,14 @@
 module.exports = () => {
 
   const {
-    API_AUTH,
-    APPROVED_COMMENTS_FORM_ID,
+    NETLIFY_AUTH_TOKEN,
     SLACK_WEBHOOK_URL,
     SITE_NAME
   } = process.env;
 
   return {
-    ready : API_AUTH && APPROVED_COMMENTS_FORM_ID && SLACK_WEBHOOK_URL ? true : false,
-    API_AUTH_ready : API_AUTH ? true : false,
-    APPROVED_COMMENTS_FORM_ID_ready : APPROVED_COMMENTS_FORM_ID ? true : false,
+    ready : NETLIFY_AUTH_TOKEN && SLACK_WEBHOOK_URL ? true : false,
+    NETLIFY_AUTH_TOKEN_ready : NETLIFY_AUTH_TOKEN ? true : false,
     SLACK_WEBHOOK_URL_ready : SLACK_WEBHOOK_URL ? true : false,
     SITE_NAME
   }
