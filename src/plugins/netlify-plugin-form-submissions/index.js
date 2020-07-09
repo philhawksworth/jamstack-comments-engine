17807c80ea34a7ef6845202066cea3157481243b
Phil Hawksworth <phil@netlify.com>
2020-07-09 11:17:23 +0100

    form name options handling

--- src/plugins/netlify-plugin-form-submissions/index.js
+++ src/plugins/netlify-plugin-form-submissions/index.js
@@ -9,6 +9,16 @@ const {
   NETLIFY_AUTH_TOKEN
 } = process.env;
 
+let formIDs = {};
+
+
+const getFormSubmissions = async function(formName, path) {
+  let formID = formIDs[formName];
+  const submissions = await fetch(`https://api.netlify.com/api/v1/forms/${formID}/submissions?access_token=${NETLIFY_AUTH_TOKEN}`).then(res => res.json());
+  await fs.writeFileSync(path, JSON.stringify(submissions));
+  console.log('Form submissions data saved:', chalk.yellow(path));
+};
+
 
 module.exports = {
 
@@ -16,10 +26,9 @@ module.exports = {
 
     // get info about what forms exist on the site
     const forms = await fetch(`https://api.netlify.com/api/v1/sites/${constants.SITE_ID}/forms?access_token=${NETLIFY_AUTH_TOKEN}`).then(res => res.json());
-    console.log(chalk.blue(`${forms.length} forms found in the site:`));
 
-    // build a map of the form names and their IDs
-    let formIDs = {};
+    // build an index object of the form names and their IDs
+    console.log(chalk.blue(`${forms.length} forms found in the site:`));
     forms.forEach(form => {
       console.log(chalk.blue(form.name), `(${form.id})` );
       formIDs[form.name] = form.id;
@@ -27,17 +36,14 @@ module.exports = {
 
 
     // get submissions to specified forms or all forms?
-    const chosenForms = inputs.formNames;
+    const chosenForms = inputs.formNames == 'ALL' ? Object.keys(formIDs) : [].concat(inputs.formNames)
 
-    // if all, fetch ids of all forms for this site
-    let id = formIDs[chosenForms];
-
-    console.log('fetch :>> ', chosenForms, id);
-
-    const submissions = await fetch(`https://api.netlify.com/api/v1/forms/${id}/submissions?access_token=${NETLIFY_AUTH_TOKEN}`).then(res => res.json());
-    const dataFilePath = `${inputs.dataDirectory}/${chosenForms}_submissions.json`;
-    await fs.writeFileSync(dataFilePath, JSON.stringify(submissions));
-    console.log('Form submissions data saved: ', chalk.yellow(dataFilePath));
+    // get submissions to each form in parallel
+    const promises = chosenForms.map((formName) => {
+      const dataFilePath = `${inputs.dataDirectory}/${formName}_submissions.json`;
+      return getFormSubmissions(formName, dataFilePath)
+    });
+    await Promise.all(promises);
 
 
   }
